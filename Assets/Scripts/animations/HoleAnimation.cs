7ab8cd5c0d1a86d0ec9337f234197e93cdb21bfa
Your Name <you@example.com>
2025-03-21 02:57:25 +0600

    feat, fix: scene1 coin animations added, animation graph bug fix

--- Assets/Scripts/animations/HoleAnimation.cs
+++ Assets/Scripts/animations/HoleAnimation.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class HoleAnimation : MonoBehaviour
 {
     private Animator _animator;
-    // Start is called before the first frame update
+    private AudioSource _aus;
+
+    [SerializeField] private List<AudioClip> clips;
+    
     void Start()
     {
         _animator = GetComponent<Animator>();
+        _aus = GetComponent<AudioSource>();
     }
 
     void OnMouseDown()
@@ -20,5 +24,17 @@ public class HoleAnimation : MonoBehaviour
     private void StopPlay()
     {
         _animator.SetBool("IsPlay", false);
+    }
+
+    public void PlayUp()
+    {
+        _aus.clip = clips[0];
+        _aus.Play();
+    }
+
+    public void PlayDown()
+    {
+        _aus.clip = clips[1];
+        _aus.Play();
     }
 }
