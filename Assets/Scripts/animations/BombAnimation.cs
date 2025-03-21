fd61e0676ea7ff7ad498f3b2eba72a69e326075f
Your Name <you@example.com>
2025-03-21 10:39:23 +0600

    feat: scene2 click animation added

--- Assets/Scripts/animations/BombAnimation.cs
+++ Assets/Scripts/animations/BombAnimation.cs
@@ -2,12 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BarrelAnimation : MonoBehaviour
+public class BombAnimation : MonoBehaviour
 {
     private Animator _animator;
     private AudioSource _aus;
 
-    private ParticleSystem particle;
+    private ParticleSystem particle1;
+    private ParticleSystem particle2;
 
     [SerializeField] private List<AudioClip> clips;
 
@@ -15,7 +16,8 @@ public class BarrelAnimation : MonoBehaviour
     {
         _animator = GetComponent<Animator>();
         _aus = GetComponent<AudioSource>();
-        particle = this.gameObject.transform.GetChild(1).GetComponent<ParticleSystem>();
+        particle1 = this.gameObject.transform.GetChild(0).GetComponent<ParticleSystem>();
+        particle2 = this.gameObject.transform.GetChild(1).GetComponent<ParticleSystem>();
     }
 
     void OnMouseDown()
@@ -30,16 +32,22 @@ public class BarrelAnimation : MonoBehaviour
     }
 
 
-    void MetalSoundPlay()
+    void FireSoundPlay()
     {
         _aus.clip = clips[0];
         _aus.Play();
+        particle1.Play();
+        particle2.Play();
     }
 
-    void PourSoundPlay()
+    void ExplosionPlay()
     {
         _aus.clip = clips[1];
         _aus.Play();
-        particle.Play();
+    }
+    void FallSoundPlay()
+    {
+        _aus.clip = clips[2];
+        _aus.Play();
     }
 }
